fc091a9ac6d7496ea57173a8d17f41ff100c1556
jarnokorstanje <jakorstan@gmail.com>
2022-08-04 11:02:30 +0200

    add all other gui methods

--- src/gui.ts
+++ src/gui.ts
@@ -1,13 +1,50 @@
+import { Message } from "./messageInterface";
+
 export class GUI {
     messageList: HTMLUListElement;
+    statusText: HTMLParagraphElement;
+    connectButton: HTMLButtonElement;
+    disconnectButton: HTMLButtonElement;
 
     constructor() {
         this.messageList = document.getElementById("messageList") as HTMLUListElement;
+        this.statusText = document.getElementById("statusText") as HTMLParagraphElement;
+        this.connectButton = document.getElementById("connectButton") as HTMLButtonElement;
+        this.disconnectButton = document.getElementById("disconnectButton") as HTMLButtonElement;
     }
 
     printListItem(text: string) {
         const li = document.createElement("li");
         li.textContent = text;
         this.messageList.appendChild(li);
+    }
+
+    printMessage(message: Message) {
+        const dateObj = new Date(message.timestamp);
+        this.printListItem(`(${dateObj.toLocaleTimeString('nl-NL')}) ${message.caller}: "${message.text}"`);
+    }
+
+    printMissedMessages(missedMessages: Message[]) {
+        if (missedMessages.length > 0) {
+            this.printListItem("Verbonden met SignalR, gemiste berichten:");
+            missedMessages.forEach(message => {
+                this.printMessage(message);
+            });
+        } else {
+            this.printListItem("Verbonden met SignalR, er zijn geen gemiste berichten");
+        }      
+    }
+
+    setConnected() {
+        this.disconnectButton.style.display = "inline";  
+        this.connectButton.style.display = "none";
+        this.statusText.style.display = "inline";
+    }
+
+    setDisconnected() {
+        this.printListItem("Verbinding verbroken");
+        this.disconnectButton.style.display = "none";  
+        this.connectButton.style.display = "inline";
+        this.statusText.style.display = "none";
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,12 @@ import * as signalR from "@microsoft/signalr";
 import { Message } from "./messageInterface";
 import { GUI } from "./gui";
 
-//TODO: Klasse voor GUI anapassingen
-//- classe voor signalR
+//TODO: - klasse voor signalR
 // - then structuren omschrijven naar async await indien mogelijk
-
 
 const userInput: HTMLInputElement  = document.getElementById("userInput") as HTMLInputElement;
 const connectButton: HTMLButtonElement = document.getElementById("connectButton") as HTMLButtonElement;
 const disconnectButton: HTMLButtonElement = document.getElementById("disconnectButton") as HTMLButtonElement;
-const statusText: HTMLParagraphElement = document.getElementById("statusText") as HTMLParagraphElement;
 const gui: GUI = new GUI();
 
 const connection = new signalR.HubConnectionBuilder()
@@ -25,9 +22,7 @@ function connect(): void {
         .then(() => {
             try {
                 connection.invoke("AddToGroup", userInput.value);
-                disconnectButton.style.display = "inline";  
-                connectButton.style.display = "none";
-                statusText.style.display = "inline";
+                gui.setConnected();
             }
             catch(error) {
                 console.error(error);
@@ -39,33 +34,20 @@ function disconnect(): void {
     connection.stop()
         .catch((error) => console.error(error))
         .then(() => {
-            gui.printListItem("Verbinding verbroken");
-            disconnectButton.style.display = "none";  
-            connectButton.style.display = "inline";
-            statusText.style.display = "none";
+            gui.setDisconnected();
         });
 }
 
 connection.on("message", (message) => {
     connection.invoke("MessageResponse", message.guid);
 
-    const dateObj = new Date(message.timestamp);
-    gui.printListItem(`(${dateObj.toLocaleTimeString('nl-NL')}) ${message.caller}: "${message.text}"`);
+    gui.printMessage(message);
 });
 
 connection.on("missedMessages", (missedMessages: Message[]) => {
-    if (missedMessages.length > 0) {
-        gui.printListItem("Verbonden met SignalR, gemiste berichten:");
-        missedMessages.forEach(message => {
-            const dateObj = new Date(message.timestamp);
-            gui.printListItem(`(${dateObj.toLocaleTimeString('nl-NL')}) ${message.caller}: "${message.text}"`);
-        });
-    } else {
-        gui.printListItem("Verbonden met SignalR, er zijn geen gemiste berichten");
-    }      
+    gui.printMissedMessages(missedMessages);
 });
 
 connectButton.addEventListener("click", connect);
 
 disconnectButton.addEventListener("click", disconnect);
-
