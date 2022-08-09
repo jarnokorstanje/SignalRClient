22f90ee081da23669c27510ea43713de90a349b9
jarnokorstanje <jakorstan@gmail.com>
2022-08-09 09:17:36 +0200

    Change index.ts to SignalRClient class

--- src/index.ts
+++ src/index.ts
@@ -2,38 +2,42 @@ import { Message } from "./message";
 import { GUI } from "./gui";
 import { SignalRConnection } from "./signalRConnection";
 
-//TODO: index klasse
+class SignalRClient {
+    private userInput: HTMLInputElement = document.getElementById("userInput") as HTMLInputElement;
+    private connectButton: HTMLButtonElement = document.getElementById("connectButton") as HTMLButtonElement;
+    private disconnectButton: HTMLButtonElement = document.getElementById("disconnectButton") as HTMLButtonElement;
+    private signalRConnection: SignalRConnection;
 
-const userInput: HTMLInputElement  = document.getElementById("userInput") as HTMLInputElement;
-const connectButton: HTMLButtonElement = document.getElementById("connectButton") as HTMLButtonElement;
-const disconnectButton: HTMLButtonElement = document.getElementById("disconnectButton") as HTMLButtonElement;
-const signalRConnection: SignalRConnection = new SignalRConnection(handleMessage, handleMissedMessages);
+    constructor() {
+        this.signalRConnection = new SignalRConnection(this.handleMessage, this.handleMissedMessages);
+        this.connectButton.addEventListener("click", () => { this.signalRConnection.connect(this.userInput.value);});
+        this.disconnectButton.addEventListener("click", () => { this.signalRConnection.disconnect();});
+    }
 
-connectButton.addEventListener("click", () => {signalRConnection.connect(userInput.value);});
+    public handleMessage = (message: Message) => {
+        this.signalRConnection.invokeMessageResponse(message.guid);
+        GUI.printMessage(message);
+    }
 
-disconnectButton.addEventListener("click", () => {signalRConnection.disconnect();});
+    public handleMissedMessages = (missedMessages: Message[]) => {
+        GUI.setConnectedStyle();
 
-function handleMessage(message: Message) {
-    signalRConnection.invokeMessageResponse(message.guid);
-    GUI.printMessage(message);
-}
+        if (missedMessages.length > 0) {
+            GUI.printMissedMessages(missedMessages);
 
-function handleMissedMessages(missedMessages: Message[]) {
-    GUI.setConnectedStyle();
+            // create array of guids of missedmessages
+            const missedMessageIds: string[] = [];
 
-    if (missedMessages.length > 0) {
-        GUI.printMissedMessages(missedMessages);
-        
-        // create array of guids of missedmessages
-        const missedMessageIds: string[] = [];
-    
-        missedMessages.forEach(message => {
-            missedMessageIds.push(message.guid);
-        });
+            missedMessages.forEach(message => {
+                missedMessageIds.push(message.guid);
+            });
 
-        // return guid of each missedmessage (to let the server know it has been received)
-        signalRConnection.invokeMissedMessagesResponse(missedMessageIds);
-    } else {
-        GUI.printListItem("Verbonden met SignalR, er zijn geen gemiste berichten");
+            // return guid of each missedmessage (to let the server know it has been received)
+            this.signalRConnection.invokeMissedMessagesResponse(missedMessageIds);
+        } else {
+            GUI.printListItem("Verbonden met SignalR, er zijn geen gemiste berichten");
+        }
     }
 }
+
+new SignalRClient();
